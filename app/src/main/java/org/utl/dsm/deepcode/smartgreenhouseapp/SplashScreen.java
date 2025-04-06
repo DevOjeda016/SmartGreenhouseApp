5433d2d2835a78dc04b19c8d6e899d52ab3a5e52
DevOjeda016 <172947644+DevOjeda016@users.noreply.github.com>
2025-04-05 22:20:41 -0600

    Update app version and splash screen
    This commit updates the app version number and modifies the splash screen to dynamically display the app version.
    
    -   Updated the app version to "1.1.10" in `strings.xml`.
    -   Removed the fixed version number from `strings-zh.xml` and `strings-en-rUS.xml`.
    -   Added a text view in the splash screen layout (`activity_splash_screen.xml`) to show the app version.
    -   Updated the `SplashScreen.java` to dynamically display the current version.

--- app/src/main/java/org/utl/dsm/deepcode/smartgreenhouseapp/SplashScreen.java
+++ app/src/main/java/org/utl/dsm/deepcode/smartgreenhouseapp/SplashScreen.java
@@ -1,8 +1,10 @@
 package org.utl.dsm.deepcode.smartgreenhouseapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
     Button btnGoForm;
+    private TextView textView5;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +28,12 @@ public class SplashScreen extends AppCompatActivity {
             return insets;
         });
         btnGoForm = findViewById(R.id.btnGoForm);
+        textView5 = findViewById(R.id.textView5);
+        textView5.setText(getString(R.string.app_versio) + " " + getString(R.string.app_version));
         btnGoForm.setOnClickListener(view -> {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
+            finish();
         });
     }
 }
